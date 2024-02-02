9dbedc47f8288d8e69e1e6823b145cbdcfaac93b
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-02-02 11:40:23 -0600

    slices

--- src/get_slice_locs.py
+++ src/get_slice_locs.py
@@ -6,9 +6,11 @@ import numpy
 import scipy.ndimage
 
 parser = argparse.ArgumentParser()
-parser.add_argument('img_niigz')
+parser.add_argument('--img_niigz')
+parser.add_argument('--axis')
 args = parser.parse_args()
 img_niigz = args.img_niigz
+axis = int(args.axis)
 
 img = nibabel.load(img_niigz)
 imgdata = img.get_fdata()
@@ -18,17 +20,15 @@ imgdata[numpy.isnan(imgdata)] = 0
 imgdata[imgdata>0] = 1
 com_ijk = scipy.ndimage.center_of_mass(imgdata)
 com_xyz = nibabel.affines.apply_affine(img.affine, com_ijk)
-com_xyz = numpy.round(com_xyz)
-print(com_xyz)
 
 # Extents
 locs = numpy.where(imgdata>0)
 min_ijk = [min(x) for x in locs]
 max_ijk = [max(x) for x in locs]
 xyz = nibabel.affines.apply_affine(img.affine, numpy.vstack([min_ijk, max_ijk]))
-xyz = numpy.round(xyz)
 min_xyz = numpy.apply_along_axis(min, 0, xyz)
 max_xyz = numpy.apply_along_axis(max, 0, xyz)
-print(min_xyz)
-print(max_xyz)
 
+# Four slice locations on each axis, 20% 40% 60% 80%
+slices = min_xyz[axis] + (max_xyz[axis] - min_xyz[axis]) * numpy.array([0.2, 0.4, 0.6, 0.8])
+print(f'{slices[0]:.0f} {slices[1]:.0f} {slices[2]:.0f} {slices[3]:.0f}')
